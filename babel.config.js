42735794e5247a7e44195b334949c4cead6a9554
Nguyen Van Tinh <nguyenvantinh06@gmail.com>
2022-05-11 15:46:46 +0700

    Setup Google AutoCompleted Library

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,19 @@ module.exports = function (api) {
   ];
   return {
     presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
     env: {
       production: {
         plugins: plugins.concat('transform-remove-console'),
--- src/screens/HomeScreen/view.js
+++ src/screens/HomeScreen/view.js
@@ -2,12 +2,52 @@ import React from 'react';
 import {View, Text, Image, SafeAreaView} from 'react-native';
 import styles from './styles';
 import NavOptions from '../../components/NavOptions';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from '@env';
+
+// navigator.geolocation = require('react-native-geolocation-service');
+
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Image
         style={{width: 100, height: 100, resizeMode: 'contain', marginLeft: 10}}
         source={{uri: 'https://links.papareact.com/gzs'}}
+      />
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+          container: {
+            flex: 0,
+            borderColor: 'grey',
+            borderWidth: 1,
+            marginHorizontal: 10,
+          },
+          textInput: {fontSize: 18},
+        }}
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+        }}
+        fetchDetails={true}
+        returnKeyType={'search'}
+        minLength={2}
+        query={{
+          key: GOOGLE_MAPS_APIKEY,
+          language: 'vi',
+        }}
+        enablePowerByContainer={false}
+        debounce={400}
+        //get Current Location
+        // currentLocation={true}
+        // currentLocationLabel="Current location"
+        requestUrl={{
+          useOnPlatform: 'web', // or "all"
+          url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api', // or any proxy server that hits https://maps.googleapis.com/maps/api
+          headers: {
+            Authorization: `an auth token`, // if required for your proxy
+          },
+        }}
       />
       <NavOptions />
     </SafeAreaView>
